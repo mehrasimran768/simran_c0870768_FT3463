6e5f6cd60e2e817ccc3a342c301ab4e8b956a724
simran mehra <simranmehra.iv@gmail.com>
2022-11-21 21:15:31 -0500

    adding module java class

---
+++ src/module.java
@@ -0,0 +1,12 @@
+public class module extends binary {
+
+    public module(arithmeticExp newLeft, arithmeticExp newRight) {
+        super(newLeft, newRight);
+    }
+
+    @Override
+    public String evaluate() {
+        return "";
+    }
+
+}
