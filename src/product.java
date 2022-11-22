a933648970dd8e9ba9a20f3e8d7a3d6021b1a6a9
simran mehra <simranmehra.iv@gmail.com>
2022-11-21 21:10:34 -0500

    updating a constructor

--- src/product.java
+++ src/product.java
@@ -1,7 +1,9 @@
-public class sum extends binary{
-    public sum(arithmeticExp newLeft, arithmeticExp newRight) {
+public class product extends binary {
+
+    public product(arithmeticExp newLeft, arithmeticExp newRight) {
         super(newLeft, newRight);
     }
+
     @Override
     public String evaluate() {
         return "";
